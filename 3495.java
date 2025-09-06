be02e6e38ad8fd999783fada6d04549f44242c25
Yash Yadav <136917800+11Yashyadav@users.noreply.github.com>
2025-09-06 18:10:44 +0530

    Minimum Operations to Make Array Elements Zero

---
+++ 3495.java
@@ -0,0 +1,31 @@
+class Solution {
+    private long solve(long a, long b) {
+        long L = 1;
+        long s = 1;
+        long res = 0;
+
+        while (L <= b) {
+            long R = 4 * L - 1;
+            long maxi = Math.max(L, a);
+            long mini = Math.min(R, b);
+
+            if (maxi <= mini) {
+                res += (mini - maxi + 1) * s;
+            }
+
+            L = L * 4;
+            s++;
+        }
+        return res;
+    }
+
+    public long minOperations(int[][] queries) {
+        long cnt = 0;
+        for (int[] q : queries) {
+            long a = q[0], b = q[1];
+            long s = solve(a, b);
+            cnt += (s + 1) / 2;
+        }
+        return cnt;
+    }
+}
