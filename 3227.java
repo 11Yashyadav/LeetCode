dea1e5ae9aacc6e2dc636d6a97cce6d2e6f0155e
Yash Yadav <136917800+11Yashyadav@users.noreply.github.com>
2025-09-12 22:40:04 +0530

    Vowels Game in a String

---
+++ 3227.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean doesAliceWin(String s) {
+
+        for(int i =0;i<s.length();i++)
+        {
+            char chr = s.charAt(i);
+            if(chr=='a'|| chr=='e'|| chr=='i'|| chr=='o'|| chr=='u')
+            {
+                return true;
+            }
+        }
+        return false;
+
+    }
+
+}
